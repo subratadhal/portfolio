a0f8870e46318e2f0cfa63d0e3c6f536f2f84343
Subrata Dhal <frontenddeves@gmail.com>
2024-05-16 13:05:20 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. subrata dhal
+          Edit <code>src/App.js</code> and save to reload. subrata dhal related changes
         </p>
         <a
           className="App-link"
