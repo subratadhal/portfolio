b9ee2d41c090bfda433ac823510359717ffa79ef
Subrata Dhal <frontenddeves@gmail.com>
2024-05-16 12:42:46 +0530

    update

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Welcome to ...
-        </p>        
+       
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
