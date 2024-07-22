0aec4966511b087c1f35766cd40c197634814187
Subrata Dhal <frontenddeves@gmail.com>
2024-07-22 23:09:25 +0530

    update about

--- src/Component/About/index.js
+++ src/Component/About/index.js
@@ -25,11 +25,30 @@ function About() {
               <div className="col">
                 <div className="about-body">
                   <p>
-                    Seasoned UI/UX designer with a decade of experience in
-                    innovation, blending design creativity with technical
-                    proficiency. Expertise in crafting user-centric interfaces,
-                    translating designs into functional and responsive
-                    interfaces, and leading cross-functional teams.
+                    I take great pride in being a UI/UX Designer because it
+                    allows me to create meaningful and impactful experiences for
+                    users. This role lets me blend creativity with analytical
+                    thinking, ensuring that every design decision not only looks
+                    good but also functions seamlessly and meets user needs. The
+                    challenge of balancing aesthetics with usability and
+                    aligning these with business objectives is both exciting and
+                    fulfilling.
+                    <br />
+                    <br />
+                    Moreover, as a UI/UX Designer, I get to be at the forefront
+                    of innovation, constantly exploring new technologies and
+                    design trends. I love the collaborative nature of this work,
+                    where I can interact with diverse teams, understand
+                    different perspectives, and create holistic solutions. Each
+                    project is an opportunity to learn, grow, and make a
+                    tangible difference in the lives of users.
+                    <br />
+                    <br />
+                    Ultimately, being a UI/UX Designer means I can contribute to
+                    creating better, more intuitive digital experiences that
+                    enhance people's daily lives. This ability to combine
+                    passion with purpose is what makes me truly proud of my
+                    profession.
                   </p>
                 </div>
               </div>
